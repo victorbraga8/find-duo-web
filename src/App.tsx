2d6e984d1322dcbbc9dec046275823aac59ae173
victorbraga <victor.medeiros@contraprova.com.br>
2023-05-27 09:11:07 -0300

    Inclusão de Dialog Trigger no Banner Game

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,59 @@
-import {useState, useEffect} from 'react';
-import * as Dialog from '@radix-ui/react-dialog';
+import { useState, useEffect } from "react";
+import * as Dialog from "@radix-ui/react-dialog";
 import "./styles/main.css";
 import logoImg from "./assets/logo.svg";
 import { GameBanner } from "./components/GameBanner";
 import { CreateAdsBanner } from "./components/CreateAdsBanner";
-import { CreateAdModal } from './components/CreateAdModal';
-import axios from 'axios';
+import { CreateAdModal } from "./components/CreateAdModal";
+import axios from "axios";
 
-interface Game{
+interface Game {
   id: string;
   title: string;
   bannerUrl: string;
-  _count:{
-    ads:number;
-  }
+  _count: {
+    ads: number;
+  };
 }
 
-function App(){
+function App() {
   const [games, setGames] = useState<Game[]>([]);
 
-  useEffect(()=>{    
-    axios('https://api-findduo.vercel.app/games')    
-    .then(response=>{
-      console.log(games)
+  useEffect(() => {
+    axios("https://api-findduo.vercel.app/games").then((response) => {
+      console.log(games);
       setGames(response.data);
-    })
+    });
   }, []);
 
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} className="max-w-sm" alt="" />
-      <h1 className="text-6xl text-white font-black mt-20">Seu <span className="bg-clip-text text-transparent bg-nlw-gradient">duo</span> está aqui</h1>
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{" "}
+        <span className="bg-clip-text text-transparent bg-nlw-gradient">
+          duo
+        </span>{" "}
+        está aqui
+      </h1>
       <div className="grid grid-cols-6 gap-6 mt-16">
-        {games.map(game=>{
-          return(
-            <GameBanner key={game.id} bannerUrl={game.bannerUrl} adsCount={game._count.ads} title={game.title}/>          
-          )
+        {games.map((game) => {
+          return (
+            <GameBanner
+              key={game.id}
+              bannerUrl={game.bannerUrl}
+              adsCount={game._count.ads}
+              title={game.title}
+            />
+          );
         })}
-          
       </div>
       <Dialog.Root>
-      <CreateAdsBanner/>
-        <CreateAdModal/>
+        <CreateAdsBanner />
+        <CreateAdModal />
       </Dialog.Root>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/GameBanner.tsx
+++ src/components/GameBanner.tsx
@@ -1,18 +1,31 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { CreateAdModal } from "./CreateAdModal";
 
-interface GameBannerProps{
+interface GameBannerProps {
   bannerUrl: string;
   title: string;
   adsCount: number;
 }
 
-export function GameBanner(props:GameBannerProps){
+export function GameBanner(props: GameBannerProps) {
   return (
-    <a href="" className="relative rounded-lg overflow-hidden">
-    <img src={props.bannerUrl} alt="" />
-    <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-      <strong className="font-bold text-white block">{props.title}</strong>            
-      <span className="text-zinc-300 text-sm block mt-1">{props.adsCount} anúncio (s)</span>
+    <div>
+      <Dialog.Root>
+        <Dialog.Trigger className=" text-white rounded flex items-center gap-3">
+          <a href="#" className="relative rounded-lg overflow-hidden">
+            <img src={props.bannerUrl} alt="" />
+            <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+              <strong className="font-bold text-white block">
+                {props.title}
+              </strong>
+              <span className="text-zinc-300 text-sm block mt-1">
+                {props.adsCount} anúncio (s)
+              </span>
+            </div>
+          </a>
+        </Dialog.Trigger>
+        <CreateAdModal />
+      </Dialog.Root>
     </div>
-  </a>
-  )
+  );
 }
